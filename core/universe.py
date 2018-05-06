a687a46cb7aad3e9657eb34424f9ba154a0100d4
Torphage <filip.torphage1@gmail.com>
2018-05-06 02:04:29 +0200

    progress

--- core/universe.py
+++ core/universe.py
@@ -7,98 +7,86 @@ from pygame.locals import *
 from scipy import constants
 
 
-class CelestialBody(pygame.sprite.Sprite):
-    def __init__(self, name, mass,
-                 position=np.zeros(2, dtype=np.longdouble),
-                 velocity=np.zeros(2, dtype=np.longdouble)):
+class Particle(pygame.sprite.Sprite):
+    def __init__(self, name, mass, position, velocity, acceleration):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.mass = mass
         self.position = position
         self.velocity = velocity
-        self.force = np.zeros(2, dtype=np.longdouble)
+        self.acceleration = acceleration
+        self.merged = False
+        self.state = State(np.random.rand(2,), np.random.rand(2,))
 
-        self.FORCE_FACTOR = constants.G * self.mass
-        self.VELOCITY_FACTOR = 100 / self.mass
+
+class State:
+    def __init__(self, position, velocity):
+        self._position = position
+        self._velocity = velocity
+
+
+class Derivative:
+    def __init__(self, velocity, acceleration):
+        self._velocity = velocity
+        self._acceleration = acceleration
 
 
 class Universe:
     def __init__(self):
-        self.i = 0
-        self.bodies = list()
+        self.particle_list = list()
+        self.dt = 1
 
-    def add_body(self, object):
-        self.bodies.append(object)
+    def add_body(self, particle):
+        self.particle_list.append(particle)
 
-    def object_movement(self, object):
-        current_movement = object.movement
+    def update_particles(self):
+        for particle in self.particle_list:
+            handler = HandleParticle(particle, self.particle_list)
+            handler.update(self.dt)
 
-    def get_distance(self, object1, object2):
-        distance = object1.position - object2.position
-        return distance
 
-    def get_gravity_force(self, object1, object2):
-        mass1 = object1.mass
-        mass2 = object2.mass
-        distance = self.get_distance(object1, object2)
-        # distance = np.linalg.norm(distance)
-        force1 = (constants.G * mass1 * mass2) / np.square(distance[0])
-        force2 = (constants.G * mass1 * mass2) / np.square(distance[1])
-        if force1 == float('inf'):
-            force1 = 0
-        if force2 == float('inf'):
-            force2 = 0
-        force = np.array([force1, force2])
-        return force
+class HandleParticle(Universe):
+    def __init__(self, particle, particle_list):
+        self.particle = particle
+        self.particle_list = particle_list
 
-    def vector_normalize(self, distance):
-        norm = np.sqrt(distance[0] ** 2 + distance[1] ** 2)
-        return norm
+    def acceleration(self, state):
+        acceleration = np.zeros([2, 2])
+        for other in self.particle_list:
+            if self.particle is other or self.particle.merged:
+                continue
+            position = other.state._position - state._position
+            distance_squared = np.square(position)
+            distance_sum = np.sum(distance_squared)
+            distance = np.sqrt(distance_sum)
+            force = (self.particle.mass *
+                     other.mass) / distance_squared
+            acceleration += (force * position) / distance
+        return acceleration
 
-    def vector_direction(self, distance, norm):
-        direction = np.array([distance[0] / norm, distance[1] / norm])
-        return direction
+    def initial_derivative(self, state):
+        acceleration = self.acceleration(state)
+        return Derivative(state._velocity, acceleration)
 
-    def vector_gravity(self, direction, norm):
-        gravtity = np.array([direction[0] * norm, direction[1] * norm])
-        return gravtity
+    def next_derivative(self, initial_state, derivative, dt):
+        state = State(np.array([0., 0.]), np.array([0., 0.]))
+        state._position = initial_state._position + derivative._velocity * dt
+        state._velocity = initial_state._velocity + derivative._acceleration * dt
+        acceleration = self.acceleration(state)
+        return Derivative(state._velocity, acceleration)
 
-    def total_vectors(self, object1, object2):
-        gravity = self.get_gravity_force(object1, object2)
-        # print(gravity)
-        # print(gravity)
-        # print(velocity)
-        return gravity
-
-    # def update_forces(self, object1, object2):
-    #     dist = object1.pos - object2.pos
-    #     vector_distance = np.linalg.norm(dist)
-
-    #     # Caculate common force of gravity
-    #     g_com = (
-    #         constants.G * object1.mass * object2.mass) / (vector_distance ** 2)
-
-    #     object1.forces -= np.array([g_com * dist])
-
-    def update(self, body):
-        """Step up the simulation based on force calculations."""
-        # Step up velocity and position
-        body.velocity += np.dot(body.force, body.VELOCITY_FACTOR)
-        body.position += np.dot(body.velocity, 100)
-
-        # Reset force profile
-        body.force = np.zeros(3, dtype=np.longdouble)
-
-    def force_g(self, body, other_body):
-        """Update the force the body has on this craft."""
-        dist = body.position - other_body.position
-        gravity_component = (body.FORCE_FACTOR * other_body.mass /
-                             ((dist[0]**2 + dist[1]**2)**1.5))
-
-        # Update forces array
-        print(dist)
-        print(gravity_component)
-        body.force -= np.dot(dist, gravity_component)
+    def update(self, dt):
+        k1 = self.initial_derivative(self.particle.state)
+        k2 = self.next_derivative(self.particle.state, k1, dt * 0.5)
+        k3 = self.next_derivative(self.particle.state, k2, dt * 0.5)
+        k4 = self.next_derivative(self.particle.state, k3, dt)
+        velocity = (1 / 6) * (k1._velocity + 2 *
+                              (k2._velocity + k3._velocity) + k4._velocity)
+        acceleration = (1 / 6) * (k1._acceleration + 2 *
+                                  (k2._acceleration
+                                   + k3._acceleration) + k4._acceleration)
+        self.particle.state._position = velocity * dt
+        self.particle.state._velocity = acceleration * dt
 
 
 def main_game():
@@ -114,9 +102,11 @@ def main_game():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
-    earth = CelestialBody('Earth', 53232, np.array([0, 0]))
+
+
+    earth = Particle('Earth', 53232, np.random.rand(2,), np.random.rand(2,), np.random.rand(2,))
     earth.position = np.array([120, 480], dtype='float64')
-    moon = CelestialBody('Moon', 9333222233, np.array([0, 0]))
+    moon = Particle('Moon', 9333222233, np.random.rand(2,), np.random.rand(2,), np.random.rand(2,))
     moon.position = np.array([480, 480], dtype='float64')
 
     universe = Universe()
@@ -124,11 +114,6 @@ def main_game():
     universe.add_body(moon)
     allsprites = pygame.sprite.RenderPlain((moon, earth))
     clock = pygame.time.Clock()
-
-    distance = universe.get_distance(earth, moon)
-
-    vector1 = earth.velocity
-    vector2 = moon.velocity
 
     earth_x, earth_y = earth.position
     earth.velocity = np.array([0, 200], dtype='float64')
@@ -146,13 +131,15 @@ def main_game():
         clock.tick(60)
         screen.fill((255, 255, 255))
 
-        for i, planet in enumerate(planets):
-            for j, other_planet in enumerate(planets):
-                if planet != other_planet:
-                    print(planet.position)
-                    universe.force_g(planet, other_planet)
+        # for i, planet in enumerate(planets):
+        #     for j, other_planet in enumerate(planets):
+        #         if planet != other_planet:
+        #             print(planet.position)
+        #             universe.force_g(planet, other_planet)
 
-            universe.update(planet)
+        #     universe.update(planet)
+        universe.update_particles()
+        print(earth.position)
         # earth.position += universe.total_vectors(earth, moon)
         # earth.velocity = universe.total_vectors(earth, moon)
 
@@ -161,8 +148,11 @@ def main_game():
 
         # earth_xv, earth_yv = earth.velocity
         # print(earth.velocity)
-        pygame.draw.circle(screen, RED, [int(earth_x), int(earth_y)], 5)
-        # pygame.draw.circle(screen, RED, [int(moon_x), int(moon_y)], 10)
+        position = earth.position.astype(int)
+        print(position)
+        pygame.draw.circle(screen, RED, position.tolist(), 5)
+        position = moon.position.astype(int)
+        pygame.draw.circle(screen, RED, position.tolist(), 10)
 
         pygame.display.flip()
         for event in pygame.event.get():
